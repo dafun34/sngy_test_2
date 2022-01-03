eaf180d6e20b4f6edadc17749661cabcec6819fa
Niki <deryuginceresit@yandex.ru>
2022-01-03 13:58:07 +0300

    Commit for review

--- backend/applicant/admin.py
+++ backend/applicant/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from .models import Occupation
 
 class OccupationAdmin(admin.ModelAdmin):
-    list_display = ('name',
+    list_display = ('id',
+                    'name',
                     'company_name',
                     'position_name')
 
--- backend/backend/schema.py
+++ backend/backend/schema.py
@@ -4,10 +4,12 @@ from graphene_django import DjangoObjectType, DjangoListField
 from applicant.models import Occupation
 from graphene_django.forms.mutation import DjangoModelFormMutation
 
+
 class OccupationType(DjangoObjectType):
     class Meta:
         model = Occupation
         fields = ('__all__')
+
 
 class Query(graphene.ObjectType):
     getOccupations = graphene.List(OccupationType)
@@ -29,6 +31,42 @@ class Query(graphene.ObjectType):
 # advance: Int!,
 # by_hours: Boolean!
 
+# class OccupationMutation(graphene.Mutation):
+#     class Arguments:
+#         name = graphene.String(required=True),
+#         company_name=graphene.String(required=True)
+#         position_name=graphene.String(required=True)
+#         hire_date=graphene.Date(required=True)
+#         fire_date=graphene.Date()
+#         salary=graphene.Int(required=True),
+#         fraction=graphene.Int(required=True)
+#         base=graphene.Int(required=True)
+#         advance=graphene.Int(required=True)
+#         by_hours=graphene.Boolean(required=True)
+#         id = graphene.ID()
+#     occupation = graphene.Field(OccupationType)
+#
+#     @classmethod
+#     def mutate(cls, root, info, name, company_name, position_name,
+#                hire_date, fire_date, salary, fraction, base, advance, by_hours,
+#                id):
+#         occupation = Occupation.objects.get(pk=id)
+#         occupation.name = name
+#         occupation.company_name = company_name
+#         occupation.position_name = position_name
+#         occupation.hire_date = hire_date
+#         occupation.fire_date = fire_date
+#         occupation.salary = salary
+#         occupation.fraction = fraction
+#         occupation.base = base
+#         occupation.advance = advance
+#         occupation.by_hours = by_hours
+#         occupation.save()
+#         return OccupationMutation(occupation=occupation)
+#
+# class Mutation(graphene.ObjectType):
+#     updateOccupation = OccupationMutation.Field()
+
 class Mutation(graphene.ObjectType):
     addOccupation = graphene.Field(OccupationType,
                                    name=graphene.String(required=True),
@@ -42,9 +80,17 @@ class Mutation(graphene.ObjectType):
                                    advance=graphene.Int(required=True),
                                    by_hours=graphene.Boolean(required=True)
                                    )
+    removeOccupation = graphene.Field(graphene.String, obj_id=graphene.ID())
 
     def resolve_addOccupation(root, info, **kwargs):
         occupation, created = Occupation.objects.get_or_create(**kwargs)
         return occupation
+
+    def resolve_removeOccupation(root, info, obj_id):
+        try:
+            Occupation.objects.get(id=obj_id).delete()
+        except Occupation.DoesNotExist:
+            return f'Объекта с ID:{obj_id} не существует'
+        return f'Объект с ID:{obj_id} успешно удален'
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
